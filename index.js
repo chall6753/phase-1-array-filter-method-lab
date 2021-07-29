046a919477422c5bbdca8772ff6b05bec4f37525
Charles <chall70101@gmail.com>
2021-07-29 10:46:08 -0600

    commit tree 955a945e222cb1cdebefeee24350f191bb358c19

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+function findMatching(arr, str){
+    const matchedNames = arr.filter(cb)
+    return matchedNames
+
+    function cb(nm){
+      return  nm.toUpperCase() === str.toUpperCase()
+        
+    }
+}
+
+
+function fuzzyMatch(arr, str){
+    const fuzzyMatchedNames = arr.filter(cb)
+    return fuzzyMatchedNames
+
+    function cb(nm){
+        let three = nm.slice(0,2)
+        return three === str
+    }
+}
+
+
+let matchName = (arr, str) => {
+    const matchNames = arr.filter(cb)
+    return matchNames
+
+    function cb(nm){
+        return nm.name === str
+    }
+}
